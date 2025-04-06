6ecf980236d23d449c3d4ad812a05ec962879144
priscila-gomez-davinci <priscila.gomez@davinci.edu.ar>
2025-04-06 18:34:34 -0300

    Initial Commit

---
+++ NFCPocTests/NFCPocTests.swift
@@ -0,0 +1,17 @@
+//
+//  NFCPocTests.swift
+//  NFCPocTests
+//
+//  Created by Priscila Gomez on 06/04/2025.
+//
+
+import Testing
+@testable import NFCPoc
+
+struct NFCPocTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
